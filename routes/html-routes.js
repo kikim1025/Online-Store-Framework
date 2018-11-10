419a7a98dc35c9be9db4d0b76834f601059a7c84
Ki <kikim1025@gmail.com>
2018-11-10 01:29:41 -0500

    Fixed typo

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,7 @@
-const models = require("path");
+const path = require("path");
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-    
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));        
     });
 }
